de5ee09f17101eb39c33bb06c665c8c1300484b1
DeLuks <dragojeviclk16@gmail.com>
2025-02-03 22:13:58 +0100

    Optimization of switch-case

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
+#include <unordered_map>
 #include <windows.h>
 
-// TODO: ы ь and ъ don't work.. also EVERYTHING IS CAPSLOCK
-// TODO: maybe also find a smarter way to do this instead of a giant switch-case...
+// TODO: ы ь and ъ don't work..
 
 HHOOK g_hook = nullptr;
 BOOL xYPressed = FALSE;
 BOOL xZPressed = FALSE;
 BOOL xCPressed = FALSE;
 BOOL xIPressed = FALSE;
+
+std::unordered_map<WPARAM, std::pair<wchar_t, wchar_t>> keymap = {
+  {'B', {L'Б', L'б'}}, {'C', {L'Ц', L'ц'}},
+  {'D', {L'Д', L'д'}}, {'F', {L'Ф', L'ф'}},
+  {'G', {L'Г', L'г'}}, {'H', {L'Х', L'х'}},
+  {'J', {L'Й', L'й'}}, {'K', {L'К', L'к'}},
+  {'L', {L'Л', L'л'}}, {'M', {L'М', L'м'}},
+  {'N', {L'Н', L'н'}}, {'P', {L'П', L'п'}},
+  {'R', {L'Р', L'р'}}, {'S', {L'С', L'с'}},
+  {'T', {L'Т', L'т'}}, {'U', {L'У', L'у'}},
+  {'V', {L'В', L'в'}}, {'Y', {L'Ы', L'ы'}},
+};
 
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam);
 VOID SendUnicodeKey(wchar_t ch);
@@ -51,177 +63,85 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
     if (kbd->vkCode == VK_ESCAPE) {
       PostQuitMessage(0);
     }
-
+    BOOL isUpper = (GetKeyState(VK_SHIFT) & 0x8000) || (GetKeyState(VK_CAPITAL) & 0x0001);
     if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
       switch (kbd->vkCode) {
       case 'A':
-        SendUnicodeKey(xYPressed ? L'Я' : L'А');
-        xYPressed = FALSE;
-        return 1;
       case 'a':
-        SendUnicodeKey(xYPressed ? L'я' : L'а');
+        if (isUpper) {
+          SendUnicodeKey(xYPressed ? L'Я' : L'А');
+        }
+        else {
+          SendUnicodeKey(xYPressed ? L'я' : L'а');
+        }
         xYPressed = FALSE;
-        return 1;
-      case 'B':
-        SendUnicodeKey(L'Б');
-        return 1;
-      case 'b':
-        SendUnicodeKey(L'б');
-        return 1;
-      case 'V':
-        SendUnicodeKey(L'В');
-        return 1;
-      case 'v':
-        SendUnicodeKey(L'в');
-        return 1;
-      case 'G':
-        SendUnicodeKey(L'Г');
-        return 1;
-      case 'g':
-        SendUnicodeKey(L'г');
-        return 1;
-      case 'D':
-        SendUnicodeKey(L'Д');
-        return 1;
-      case 'd':
-        SendUnicodeKey(L'д');
         return 1;
       case 'E':
-        SendUnicodeKey(xYPressed ? L'Е' : L'Э');
-        xYPressed = FALSE;
-        return 1;
       case 'e':
-        SendUnicodeKey(xYPressed ? L'е' : L'э');
+        if (isUpper) {
+          SendUnicodeKey(xYPressed ? L'Е' : L'Э');
+        }
+        else {
+          SendUnicodeKey(xYPressed ? L'е' : L'э');
+        }
         xYPressed = FALSE;
         return 1;
       case 'Z':
-        if (xZPressed) {
-          SendUnicodeKey(L'З');
-          xZPressed = FALSE;
-          return 1;
-        }
-        xZPressed = TRUE;
-        return 1;
       case 'z':
         if (xZPressed) {
-          SendUnicodeKey(L'з');
+          SendUnicodeKey(isUpper ? L'З' : L'з');
           xZPressed = FALSE;
           return 1;
         }
         xZPressed = TRUE;
         return 1;
       case 'I':
-        if (xIPressed) {
-          SendUnicodeKey(L'И');
-          xIPressed = FALSE;
-          return 1;
-        }
-        xIPressed = TRUE;
-        return 1;
       case 'i':
         if (xIPressed) {
-          SendUnicodeKey(L'и');
+          SendUnicodeKey(isUpper ? L'И' : L'и');
           xIPressed = FALSE;
           return 1;
         }
         xIPressed = TRUE;
         return 1;
-      case 'J':
-        SendUnicodeKey(L'Й');
-        return 1;
-      case 'j':
-        SendUnicodeKey(L'й');
-        return 1;
-      case 'K':
-        SendUnicodeKey(L'К');
-        return 1;
-      case 'k':
-        SendUnicodeKey(L'к');
-        return 1;
-      case 'L':
-        SendUnicodeKey(L'Л');
-        return 1;
-      case 'l':
-        SendUnicodeKey(L'л');
-        return 1;
-      case 'M':
-        SendUnicodeKey(L'М');
-        return 1;
-      case 'm':
-        SendUnicodeKey(L'м');
-        return 1;
-      case 'N':
-        SendUnicodeKey(L'Н');
-        return 1;
-      case 'n':
-        SendUnicodeKey(L'н');
-        return 1;
       case 'O':
-        SendUnicodeKey(xYPressed ? L'Ё' : L'О');
-        xYPressed = FALSE;
-        return 1;
       case 'o':
-        SendUnicodeKey(xYPressed ? L'ё' : L'о');
+        if (isUpper) {
+          SendUnicodeKey(xYPressed ? L'Ё' : L'О');
+        }
+        else {
+          SendUnicodeKey(xYPressed ? L'ё' : L'о');
+        }
         xYPressed = FALSE;
-        return 1;
-      case 'P':
-        SendUnicodeKey(L'П');
-        return 1;
-      case 'p':
-        SendUnicodeKey(L'п');
-        return 1;
-      case 'R':
-        SendUnicodeKey(L'Р');
-        return 1;
-      case 'r':
-        SendUnicodeKey(L'р');
-        return 1;
-      case 'S':
-        SendUnicodeKey(L'С');
-        return 1;
-      case 's':
-        SendUnicodeKey(L'с');
-        return 1;
-      case 'T':
-        SendUnicodeKey(L'Т');
-        return 1;
-      case 't':
-        SendUnicodeKey(L'т');
         return 1;
       case 'U':
-        SendUnicodeKey(xYPressed ? L'Ю' : L'У');
-        xYPressed = FALSE;
-        return 1;
       case 'u':
-        SendUnicodeKey(xYPressed ? L'ю' : L'у');
+        if (isUpper) {
+          SendUnicodeKey(xYPressed ? L'Ю' : L'У');
+        }
+        else {
+          SendUnicodeKey(xYPressed ? L'ю' : L'у');
+        }
         xYPressed = FALSE;
-        return 1;
-      case 'F':
-        SendUnicodeKey(L'Ф');
-        return 1;
-      case 'f':
-        SendUnicodeKey(L'ф');
         return 1;
       case 'H':
-        SendUnicodeKey(xZPressed ? L'Ж' : xCPressed ? L'Ч' : L'Х');
-        xZPressed = FALSE;
-        xCPressed = FALSE;
-        return 1;
       case 'h':
-        SendUnicodeKey(xZPressed ? L'ж' : xCPressed ? L'ч' : L'х');
+        if (isUpper) {
+          SendUnicodeKey(xZPressed ? L'Ж' : xCPressed ? L'Ч' : L'Х');
+        }
+        else {
+          SendUnicodeKey(xZPressed ? L'ж' : xCPressed ? L'ч' : L'х');
+        }
         xZPressed = FALSE;
         xCPressed = FALSE;
         return 1;
       case 'C':
+      case 'c':
         if (xCPressed) {
-          SendUnicodeKey(L'Ц');
+          SendUnicodeKey(isUpper ? L'Ц' : L'ц');
           xCPressed = FALSE;
-          return 1;
         }
         xCPressed = TRUE;
-        return 1;
-      case 'c':
-        SendUnicodeKey(L'ц');
         return 1;
       case VK_BACK:
         if (xYPressed) {
@@ -244,6 +164,11 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
       case '#':
         SendUnicodeKey(L'ъ');
       default:
+        auto it = keymap.find(kbd->vkCode);
+        if (it != keymap.end()) {
+          SendUnicodeKey(isUpper ? it->second.first : it->second.second);
+          return 1;
+        }
         xYPressed = FALSE; // Reset if any other key is pressed
         xZPressed = FALSE;
         xCPressed = FALSE;
